e1a9b7284b7417387e72fe9c309a05ffe89f25fe
w13n <101300889+w13n@users.noreply.github.com>
2024-12-06 21:20:17 -0500

    found button style

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use crate::vc::ViewController;
 use std::error::Error;
+use iced::Theme;
+use iced::theme::Palette;
 
 mod api;
 mod error;
@@ -7,7 +9,21 @@ mod fantasy_season;
 mod vc;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    iced::run("test", ViewController::update, ViewController::view).expect("TODO: panic message");
+    iced::application("test", ViewController::update, ViewController::view)
+        .theme(|_| {
+            Theme::custom(
+                String::from("Eunomia"),
+                Palette {
+                    background: iced::color!(0x423E3B),
+                    text: iced::color!(0xFCF7F8),
+                    primary: iced::color!(0xFF6319),
+                    success: iced::color!(0x64A6BD),
+                    danger: iced::color!(0xB4869F),
+                },
+            )
+        })
+        .run()
+        .expect("TODO: panic message");
 
     Ok(())
 }
--- src/vc/style.rs
+++ src/vc/style.rs
@@ -1,26 +1,32 @@
 pub mod button {
-    use iced::Background::Color;
-    use iced::border;
+    use iced::{Background, border, Shadow, Vector};
+    use iced::Color;
     use iced::Theme;
     use iced::theme::palette::Pair;
     use iced::widget::button::{Status, Style};
 
-    fn style(background: Pair, border: Pair) -> Style {
+    fn style(background: Pair, shadow: bool) -> Style {
         Style {
-            background: Some(Color(background.color)),
+            background: Some(Background::Color(background.color)),
             text_color: background.text,
-            border: border::rounded(10).color(border.color).width(5),
-            shadow: Default::default(),
+            border: border::rounded(8),
+            shadow: if shadow {
+                Shadow {
+                    color: Color::BLACK,
+                    offset: Vector::new(0.0, 3.0),
+                    blur_radius: 1.0,
+                }
+            } else {Default::default()},
         }
     }
     pub fn primary(theme: &Theme, status: Status) -> Style {
         let palette = theme.extended_palette();
 
         match status {
-            Status::Active => style(palette.primary.base, palette.primary.weak),
-            Status::Hovered => style(palette.primary.base, palette.primary.base),
-            Status::Pressed => style(palette.primary.weak, palette.primary.weak),
-            Status::Disabled => style(palette.primary.base, palette.primary.base),
+            Status::Active => style(palette.primary.base, true),
+            Status::Hovered => style(palette.primary.strong, true),
+            Status::Pressed => style(palette.primary.strong, false),
+            Status::Disabled => style(palette.background.strong, false),
         }
     }
 }
